efca0834bcc221dcc501ba0adb04461203b10992
Dinesh <dineshm.csit@gmail.com>
2018-10-29 17:51:27 -0400

    Completed project with dataset 1

---
+++ install-ubuntu.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+sudo apt-get update
+git clone https://github.com/uWebSockets/uWebSockets 
+cd uWebSockets
+git checkout e94b6e1
+mkdir build
+cd build
+cmake ..
+make 
+sudo make install
+cd ../..
+sudo ln -s /usr/lib64/libuWS.so /usr/lib/libuWS.so
+sudo rm -r uWebSockets
